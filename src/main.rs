b33d752fd976020260f52d7d1bb1fe5ed12e2b36
ogata-k <ogtkzk712@gmail.com>
2019-01-10 23:06:19 +0900

    デバッグ情報のコメントアウト

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
                     .parent()
                     .unwrap()
                     .join("data");
-    println!("{:?}内のデータを読み込んでいます。", data_folder);
+    // println!("{:?}内のデータを読み込んでいます。", data_folder);
     
     let about_msg: &str = &format!("このプログラムはおみくじを一度引くだけのプログラムです。\nおみくじの内容は拡張子がomkjとなるファイル(以下omkjファイル)に空行無しで各行を0~6(0が最悪)の数字と伝えたいメッセージを空白区切りで指定できます。omkjファイルのファイル名が表示させたい名前になります。(例えば金運.omkj)\nomkjファイルは{:?}に配置してください。", data_folder);
     let app = App::new("omikuji")
